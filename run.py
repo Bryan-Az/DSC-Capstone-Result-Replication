640be10df6383d3593b3669421a0698f331c6315
Bryan-Az <baambriz@ucsd.edu>
2022-03-09 22:37:54 -0800

    added training to run.py with train keyword

--- run.py
+++ run.py
@@ -11,10 +11,61 @@ import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 from src.model import GENConv_Classifier 
 
+sys.path.append(os.path.realpath('./config'))
+
 from src.etl import get_data, read_test, load_graph 
 import numpy as np
 
 import pandas as pd
+
+#imports
+import torch
+import torch.nn as nn
+import torch_geometric
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from tqdm.notebook import tqdm
+import numpy as np
+import os.path as osp
+
+# +
+from torch_geometric.nn import GENConv
+
+from torch.nn import Sequential as Seq, Linear as Lin, ReLU, BatchNorm1d
+from torch_scatter import scatter_mean
+# -
+
+import tensorflow.keras as keras
+import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+import uproot
+from config.utils import *
+import tensorflow
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, BatchNormalization, GlobalAveragePooling1D
+import tensorflow.keras.backend as K
+from tqdm.notebook import tqdm
+import XRootD
+
+from config.GraphDataset import GraphDataset
+
+# +
+import yaml
+
+with open('./config/definitions.yml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    definitions = yaml.load(file, Loader=yaml.FullLoader)
+    
+features = definitions['features']
+spectators = definitions['spectators']
+labels = definitions['labels']
+
+nfeatures = definitions['nfeatures']
+nspectators = definitions['nspectators']
+nlabels = definitions['nlabels']
+ntracks = definitions['ntracks']
+# -
 
 # ## Model is loaded from pretrained .pth file
 
@@ -28,6 +79,52 @@ filename = os.path.abspath(filename)
 
 # -
 
+@torch.no_grad()
+def test(model, loader, total, batch_size, leave=False):
+    model.eval()
+    
+    #xentropy = nn.CrossEntropyLoss(reduction='mean')
+    xentropy = nn.BCEWithLogitsLoss()
+
+    sum_loss = 0.
+    #tqdm creates progress bar based on the batch
+    t = tqdm(enumerate(loader), total=total/batch_size, leave=leave)
+    for i, data in t:
+        data = data.to(device)
+        batch_output = model(data.x, data.edge_index, data.batch)
+        batch_output = batch_output.float()
+        batch_loss_item = xentropy(batch_output, data.y.float()).item()
+        sum_loss += batch_loss_item
+        t.set_description("loss = %.5f" % (batch_loss_item))
+        t.refresh() # to show immediately the update
+
+    return sum_loss/(i+1)
+
+
+def train(model, optimizer, loader, total, batch_size, leave=False):
+    model.train()
+
+    #xentropy = nn.CrossEntropyLoss(reduction='mean')
+    xentropy = nn.BCEWithLogitsLoss()
+
+    sum_loss = 0.
+    t = tqdm(enumerate(loader), total=total/batch_size, leave=leave)
+    for i, data in t:
+        data = data.to(device)
+        optimizer.zero_grad()
+        batch_output = model(data.x, data.edge_index, data.batch)
+        batch_output = batch_output.float()
+        batch_loss = xentropy(batch_output, data.y.float())
+        batch_loss.backward()
+        batch_loss_item = batch_loss.item()
+        t.set_description("loss = %.5f" % batch_loss_item)
+        t.refresh() # to show immediately the update
+        sum_loss += batch_loss_item
+        optimizer.step()
+
+    return sum_loss/(i+1)
+
+
 def main(targets):
     '''
     Runs the main project pipeline logic, given the targets.
@@ -35,24 +132,51 @@ def main(targets):
     
     `main` runs the target 'test'.
     '''
-    test_dataset = load_graph(train=False)
     batch_size = 32
-    test_samples = len(test_dataset)
     #files for test and train in ~/teams
     with open('./config/data-params.json') as fh:
         data_cfg = json.load(fh)
 
     # make the data target (all)
-    #train_generator, vtrain_generator, test_generator = get_data(**data_cfg)
-    # make the data target (testing)
-    test_loader = read_test(test_dataset)
+    train_generator, vtrain_generator, test_generator = get_data(**data_cfg)
+    test_samples = test_generator.dataset.len()
+    train_samples = len(train_generator.dataset)
+    
+    model = GENConv_Classifier().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    criterion = torch.nn.BCEWithLogitsLoss()
+    if 'train' in targets:
+        
+        n_epochs = 10
+        stale_epochs = 0
+        best_valid_loss = 99999
+        patience = 5
+        t = tqdm(range(0, n_epochs))
+
+        for epoch in t:
+            loss = train(model, optimizer, train_generator, train_samples, batch_size, leave=bool(epoch==n_epochs-1))
+            valid_loss = test(model, valid_loader, valid_samples, batch_size, leave=bool(epoch==n_epochs-1))
+            print('Epoch: {:02d}, Training Loss:   {:.4f}'.format(epoch, loss))
+            print('           Validation Loss: {:.4f}'.format(valid_loss))
+
+            if valid_loss < best_valid_loss:
+                best_valid_loss = valid_loss
+                modpath = osp.join('GENConv_model_best.pth')
+                print('New best model saved to:',modpath)
+                torch.save(model.state_dict(),modpath)
+                stale_epochs = 0
+            else:
+                print('Stale epoch')
+                stale_epochs += 1
+            if stale_epochs >= patience:
+                print('Early stopping after %i stale epochs'%patience)
+                break          
+        
     if 'test' in targets:
-        model = GENConv_Classifier()
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
         checkpoint = torch.load(filename)
         model.load_state_dict(checkpoint)
         model.eval()
-        t = tqdm(enumerate(test_loader),total=test_samples/batch_size)
+        t = tqdm(enumerate(test_generator),total=test_samples/batch_size)
         y_test = []
         y_predict = []
         spec_gnn = []
--- src/etl.py
+++ src/etl.py
@@ -23,9 +23,14 @@ fileDir = os.path.dirname(os.path.realpath('__definitions.yml__'))
 filename = os.path.join(fileDir, './config/definitions.yml')
 
 filename = os.path.abspath(filename)
+
+
 # -
 
-filename
+def collate(items):
+        l = sum(items, [])
+        return Batch.from_data_list(l)
+
 
 with open(filename) as file:
     # The FullLoader parameter handles the conversion from YAML
@@ -41,93 +46,40 @@ nspectators = definitions['nspectators']
 nlabels = definitions['nlabels']
 ntracks = definitions['ntracks']
 
+train_dir = '~/teams/DSC180A_FA21_A00/a11/train/'
+test_dir = '~/teams/DSC180A_FA21_A00/a11/test/'
+
+batch_size = 32
+
+
 # ## Create and load graph dataset
 
-def load_graph(local = True, train = True):
-    # Load Dataset
-    if local:
-        file_names = ['~/teams/DSC180A_FA21_A00/a11/train/ntuple_merged_10.root']
-        file_names_test = ['~/teams/DSC180A_FA21_A00/a11/test/ntuple_merged_0.root']
-    else:
-        file_names = ['root://eospublic.cern.ch//eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/train/ntuple_merged_10.root']
-        file_names_test = ['root://eospublic.cern.ch//eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_0.root']
-    
+def load_graph(filenames, train = True):
     if train:
-        graph_dataset = GraphDataset('gdata_train', features, labels, spectators, n_events=8000, n_events_merge=1, 
-                                     file_names=file_names)
+        graph_dataset = GraphDataset('gdata_train', features, labels, spectators, n_events=15000, n_events_merge=1, 
+                                     file_names=filenames)
         return graph_dataset
     else:
-        test_dataset = GraphDataset('gdata_test', features, labels, spectators, n_events_merge=1, 
-                                     file_names=file_names_test)
+        test_dataset = GraphDataset('gdata_test', features, labels, spectators, n_events=15000, n_events_merge=1, 
+                                     file_names=filenames)
         return test_dataset
-
-
-# ## Generators are created all in one function (TODO)
-
-#returns train/test/validation generator tuple
-def create_generators(graph):
-    
-    def collate(items):
-        l = sum(items, [])
-        return Batch.from_data_list(l)
-
-    torch.manual_seed(0)
-    valid_frac = 0.20
-    full_length = len(graph_dataset)
-    valid_num = int(valid_frac*full_length)
-    batch_size = 32
-
-    train_dataset, valid_dataset = random_split(graph_dataset, [full_length-valid_num,valid_num])
-
-    train_loader = DataListLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
-    train_loader.collate_fn = collate
-    valid_loader = DataListLoader(valid_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
-    valid_loader.collate_fn = collate
-    test_loader = DataListLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
-    test_loader.collate_fn = collate
-    
-    return (train_loader, test_loader, valid_loader)
 
 
 # ## Testing is done using the pretrained model
 
 #returns test generator
-def read_test(graph):
+def read_test(fp):
     '''
     Reads raw test data from disk.
     (Would normally be more complicated!)
     '''
-    def collate(items):
-        l = sum(items, [])
-        return Batch.from_data_list(l)
-    torch.manual_seed(0)
-    valid_frac = 0.20
-    full_length = len(graph)
-    valid_num = int(valid_frac*full_length)
-    batch_size = 32
-    
-    test_loader = DataListLoader(graph, batch_size=batch_size, pin_memory=True, shuffle=False)
+    test_dataset = load_graph(filenames=fp,train=False)
+    test_loader = DataListLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
     test_loader.collate_fn = collate
     return test_loader
 
 
-# ## TODO: Enable the training and loading of data
-
-# +
-
-#returns tuple of (train, test) data
-def get_data(train_file, vtrain_file, test_file):
-    '''
-    create generators for train/test 
-    '''
-    train_fp = os.path.join(train_dir, train_file)
-    vtrain_fp = os.path.join(train_dir, vtrain_file)
-    test_fp = os.path.join(test_dir, test_file)
-
-    return (read_train(train_fp), read_train(vtrain_fp), read_test(test_fp))
-
-
-# -
+# ## Training writes best model weights for use in testing
 
 #returns train generator
 def read_train(fp):
@@ -135,10 +87,32 @@ def read_train(fp):
     Reads raw training data from disk.
     (Would normally be more complicated!)
     '''
-    file_set = [fp]
+    graph_dataset = load_graph(filenames=fp, train=True)
+    torch.manual_seed(0)
+    valid_frac = 0.20
+    full_length = len(graph_dataset)
+    valid_num = int(valid_frac*full_length)
     
-    train_generator = DataGenerator(file_set, features, labels, spectators, batch_size=1024, n_dim=ntracks, 
-                                remove_mass_pt_window=False, 
-                                remove_unlabeled=True, max_entry=8000)
+    train_dataset, valid_dataset = random_split(graph_dataset, [full_length-valid_num,valid_num])
+    train_loader = DataListLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+    train_loader.collate_fn = collate
+    valid_loader = DataListLoader(valid_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
+    valid_loader.collate_fn = collate
+    return train_loader, valid_loader
 
-    return train_generator
+
+# +
+
+#returns tuple of (train, test) data
+def get_data(train_file, test_file):
+    '''
+    create generators for train/test 
+    '''
+    train_fp = [os.path.join(train_dir, train_file)]
+    test_fp = [os.path.join(test_dir, test_file)]
+    train_loader, valid_loader = read_train(train_fp)
+    test_loader = read_test(test_fp)
+    return (train_loader, valid_loader, test_loader)
+# -
+
+
